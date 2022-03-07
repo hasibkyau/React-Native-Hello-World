d09af7a4190fce9b71fe05350e9377374c7383e2
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-07 09:39:11 +0600

    Initialized app with 1 input field and a button

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
+import ListItem from './components/ListItem';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [placeList, setPlaceList] =useState([]);
+
+  const list = placeList.map((item, i) => {
+    console.log(item);
+    return (
+      <ListItem 
+      key = {i}
+      placeName = {item}
+      onItemPressed={()=>alert(item)}
+      />
+    )
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // View = div
+    <View style={styles.container}> 
+    {/* Text = p tag */}
+      <View style={styles.inputView}>
+        <TextInput
+        style = {{
+          width: 200,
+          borderBottomWidth: 1,
+          borderColor: "green",
+          padding: 7
+        }}
+        placeholder='Add a Place...'
+        value={inputValue}
+        onChangeText = {text => setInputValue(text)}
+        />
+        <Button
+        onPress={() => {
+          if(inputValue != ""){
+            setPlaceList([...placeList, inputValue])
+          }
+          
+        }}
+        title='Add'
+        />
+      </View> 
+      <View style = {{
+        width:"100%"
+      }}>
+        {list}
+      </View>
+    <StatusBar style="auto" />
     </View>
   );
 }
@@ -15,6 +59,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    flexDirection : 'column'
   },
+  inputView:{
+    padding: 20,
+    width: '80%',
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent:'space-between',
+    alignItems:'center'
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet, Touchable, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity } from 'react-native';
+
+const ListItem = props => {
+    return (
+        <TouchableOpacity onLongPress={props.onItemPressed}>
+            <View  style={styles.listItem}>
+                <Text>
+                    {props.placeName}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    listItem: {
+        width: "100%",
+        padding: 10,
+        backgroundColor: "#eee",
+        margin: 5
+    }
+})
+export default ListItem;
