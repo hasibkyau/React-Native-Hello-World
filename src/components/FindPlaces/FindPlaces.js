902b32477f6514801ee49eefda77ccbd1f22363f
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-07 16:55:27 +0600

    tab navigation

---
+++ src/components/FindPlaces/FindPlaces.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { shallowEqual } from "react-redux";
+
+const FindPlaces = props =>{
+    return(
+        <View>
+            <Text>Find Places</Text>
+        </View>
+    );
+}
+
+export default FindPlaces;
--- src/components/navigationTab/navigationTab.js
+++ src/components/navigationTab/navigationTab.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SharePlaces from "../ShatePlaces/SharePlaces";
+import FindPlaces from "../FindPlaces/FindPlaces";
+
+const Tab = createBottomTabNavigator();
+
 
 const navigationTab = pros =>{
     return(
-        <View>
-            <Text>Navigation Tab</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Share places" component={SharePlaces}/>
+            <Tab.Screen name="FInd Places" component={FindPlaces}/>
+        </Tab.Navigator>
     )
 }
 
