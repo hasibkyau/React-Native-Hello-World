fe5be1589ad74ae4f52f055ce05481fa2246adcd
hasibkyau <hasibkyau.cse@gmail.com>
2022-03-07 17:07:59 +0600

    addng icon in tab

--- src/components/navigationTab/navigationTab.js
+++ src/components/navigationTab/navigationTab.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SharePlaces from "../ShatePlaces/SharePlaces";
 import FindPlaces from "../FindPlaces/FindPlaces";
+import Icons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,8 +11,24 @@ const Tab = createBottomTabNavigator();
 const navigationTab = pros =>{
     return(
         <Tab.Navigator>
-            <Tab.Screen name="Share places" component={SharePlaces}/>
-            <Tab.Screen name="FInd Places" component={FindPlaces}/>
+            <Tab.Screen 
+            name="Share places" 
+            component={SharePlaces}
+            options={{
+                tabBarIcon:({color, size}) =>(
+                    <Icons name="ios-share" color={color} size={size}/>
+                )
+            }}
+            />
+            <Tab.Screen 
+            name="FInd Places" 
+            component={FindPlaces}
+            options={{
+                tabBarIcon:({color, size}) =>(
+                    <Icons name="md-map" color={color} size={size}/>
+                )
+            }}
+            />
         </Tab.Navigator>
     )
 }
